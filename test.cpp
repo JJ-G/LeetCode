a7b3d642d8a923ed0360c62f3e61b796f2b6d650
Jiajing-Guo <1264683024@qq.com>
2017-03-01 21:04:35 +0800

    Revert "Revert "test""
    This reverts commit a2114c6970a42c899bfdcf237d1c4654b7847a66.

--- test.cpp
+++ test.cpp
@@ -1,10 +1,9 @@
-#include <stdio.h>
-int main(int argc, char **argv)
+#include<iostream>
+
+using namespace std;
+
+int main()
 {
-    char name[16] = "";
-    unsigned  int  age=0;
-    printf("Plaese input your name and age:");
-    scanf("%s%d",name,&age);
-    printf("you name is:%s,age is:%d\n",name,age);
-return 0;
+	cout << "HelloWorld" << endl;
+	return 0;
 }
